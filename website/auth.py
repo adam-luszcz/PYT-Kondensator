1414cb468c44332bfcd73a0ae42b3faa9892458e
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-21 00:55:54 +0200

    added login form with wtforms

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template
+from website.forms import LoginForm
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
-def home():
-    return render_template('login.html')
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return 'Form successfully submitted!'
+    return render_template('login.html', form=form)
