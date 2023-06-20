c0bd96fe5ca82ba64aadd9b33eadc00bc5866056
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-21 00:14:45 +0200

    added login route

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'Xo6h3HsJqPx40vkoBThH22hxy3u3jFDtm4UUHI39'
 
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
