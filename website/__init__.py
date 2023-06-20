836b5b9f118f1f7b219ee52f6cfc46bd3473e3da
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-20 23:59:23 +0200

    added base bootstrap template with navbar

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 
 def create_app():
     app = Flask(__name__)
+    Bootstrap(app)
     app.config['SECRET_KEY'] = 'Xo6h3HsJqPx40vkoBThH22hxy3u3jFDtm4UUHI39'
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
 
     return app
