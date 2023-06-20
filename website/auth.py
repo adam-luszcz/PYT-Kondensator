c6932d8c7bca97424b5cb8088d6ca36da6f60ba0
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-21 01:59:59 +0200

    added form validation with messages visible to user

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash
 from website.forms import LoginForm, SignUpForm
 
 auth = Blueprint('auth', __name__)
@@ -7,14 +7,26 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        return 'Form successfully submitted!'
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = request.form.get('email')
+            password = request.form.get('password')
+            return redirect('/')
+        elif form.errors:
+            for message in form.errors.values():
+                flash(message[0], category='error')
     return render_template('login.html', form=form)
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     form = SignUpForm()
-    if form.validate_on_submit():
-        return 'Form successfully submitted!'
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = request.form.get('email')
+            password = request.form.get('password')
+            return redirect('/')
+        elif form.errors:
+            for message in form.errors.values():
+                flash(message[0], category='error')
     return render_template('signup.html', form=form)
