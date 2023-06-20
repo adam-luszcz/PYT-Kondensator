a61a7285095e0e8f3bfc1c77238e16cea19965fc
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-21 00:54:19 +0200

    removed unused flask_bootstrap package

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 
 def create_app():
     app = Flask(__name__)
-    Bootstrap(app)
     app.config['SECRET_KEY'] = 'Xo6h3HsJqPx40vkoBThH22hxy3u3jFDtm4UUHI39'
 
     from .views import views
