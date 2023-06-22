c71f414314e9a16d88e1c2b70c925c29c08019d4
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-22 19:23:09 +0200

    added topic view and template

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from .models import Topic
+from .models import Topic, Thread
 
 views = Blueprint('views', __name__)
 
@@ -10,3 +10,10 @@ views = Blueprint('views', __name__)
 def home():
     topics = Topic.query.all()
     return render_template('home.html', user=current_user, topics=topics)
+
+
+@views.route('/topic/<int:topic_id>')
+@login_required
+def topic_view(topic_id):
+    threads = Thread.query.filter_by(topic_id=topic_id).all()
+    return render_template('topic.html', user=current_user, threads=threads)
