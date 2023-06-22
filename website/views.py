b24cdc5a7a4ac6e606c926db29dbd669696bf12e
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-22 19:15:28 +0200

    now passing all topics to home template

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .models import Topic
 
 views = Blueprint('views', __name__)
 
@@ -7,4 +8,5 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    return render_template('base.html', user=current_user)
+    topics = Topic.query.all()
+    return render_template('home.html', user=current_user, topics=topics)
