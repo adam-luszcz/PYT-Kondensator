beeccdfb985845ef2075271fdfe1a29bdeb57ba6
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-22 20:12:03 +0200

    now passing more information to topic and thread templates

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from .models import Topic, Thread, Post
+from .models import Topic, Thread, Post, User
 
 views = Blueprint('views', __name__)
 
@@ -15,12 +15,34 @@ def home():
 @views.route('/topic/<int:topic_id>')
 @login_required
 def topic_view(topic_id):
+    topic = Topic.query.get_or_404(topic_id)
     threads = Thread.query.filter_by(topic_id=topic_id).all()
-    return render_template('topic.html', user=current_user, threads=threads)
+    thread_list = []
+    for thread in threads:
+        user = User.query.get(thread.user_id)
+        thread_dict = {
+            'id': thread.id,
+            'title': thread.title,
+            'author_email': user.email,
+            'created_at': thread.date
+        }
+        thread_list.append(thread_dict)
+    return render_template('topic.html', user=current_user, threads=thread_list)
 
 
 @views.route('/thread/<int:thread_id>')
 @login_required
 def thread_view(thread_id):
+    thread = Thread.query.get_or_404(thread_id)
     posts = Post.query.filter_by(thread_id=thread_id).all()
-    return render_template('thread.html', user=current_user, posts=posts)
+    post_list = []
+    for post in posts:
+        user = User.query.get(post.user_id)
+        post_dict = {
+            'id': post.id,
+            'content': post.content,
+            'author_email': user.email,
+            'created_at': post.date
+        }
+        post_list.append(post_dict)
+    return render_template('thread.html', user=current_user, posts=post_list)
