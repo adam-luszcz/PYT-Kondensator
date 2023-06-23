664713c2040112c36211cb81ee1739725c9a416d
Adam Luszcz <adam.luszcz20@gmail.com>
2023-06-23 21:05:35 +0200

    added get-all-posts route

--- website/post.py
+++ website/post.py
@@ -8,7 +8,7 @@ post = Blueprint('post', __name__)
 @post.route('/thread/<int:thread_id>/get-posts')
 def get_posts(thread_id):
     posts = Post.query.filter_by(thread_id=thread_id).all()
-    post_list = [{'id': post.id, 'content': post.content, 'thread_id': post.thread_id} for post in posts]
+    post_list = [{'id': post.id, 'content': post.content, 'date': post.date, 'user_id': post.user_id, 'thread_id': post.thread_id} for post in posts]
     return jsonify(post_list)
 
 
@@ -19,3 +19,10 @@ def add_post(thread_id):
     db.session.add(new_post)
     db.session.commit()
     return jsonify({'id': new_post.id, 'content': new_post.content}), 201
+
+
+@post.route('/get-all-posts')
+def get_all_posts():
+    posts = Post.query.all()
+    post_list = [{'id': post.id, 'content': post.content, 'date': post.date, 'user_id': post.user_id, 'thread_id': post.thread_id, } for post in posts]
+    return jsonify(post_list)
